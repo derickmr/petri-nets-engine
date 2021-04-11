00347b2ddefecef7c87dd0dc022ac3dbb42f29b7
Nathan Schneider <nathan.schneider@sap.com>
2021-04-11 17:54:32 -0300

    Add JUnit 4

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class MainTest {
+    @Test
+    public void testHelloEmpty()
+    {
+        Arc arc = new Arc();
+        assertNotNull(arc);
+    }
+}
