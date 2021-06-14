f112e44f80b1540329ed65b304bf161c246cebeb
Derick Rentz <derickrentz@pop-os.localdomain>
2021-06-13 21:01:19 -0300

    Removing entity from queue

--- src/main/java/com/unisinos/modelsimulator/restaurante/events/AtendimentoCaixa.java
+++ src/main/java/com/unisinos/modelsimulator/restaurante/events/AtendimentoCaixa.java
@@ -5,15 +5,12 @@ import org.apache.commons.math.MathException;
 
 public class AtendimentoCaixa extends Event {
 
-    private Entity grupo;
-
     public AtendimentoCaixa(String name) {
         super(name);
     }
 
     public AtendimentoCaixa(String name, Resource caixa, Entity grupo, EntitySet filaCaixa, Scheduler scheduler) {
         super(name);
-        this.grupo = grupo;
         this.resource = caixa;
         this.entitySet = filaCaixa;
         this.scheduler = scheduler;
@@ -22,20 +19,17 @@ public class AtendimentoCaixa extends Event {
     @Override
     public void execute() {
         super.execute();
-        if (grupo != null || !getEntitySet().isEmpty()) {
+        if (!getEntitySet().isEmpty()) {
 
             if (getResource().allocate(1)) { //conseguiu alocar caixa pra atender
                 //Agenda final do atendimento em normal (8,2) minutos
                 try {
+                    Entity grupo = entitySet.remove();
                     Scheduler scheduler = getScheduler();
                     scheduler.scheduleIn(scheduler.createEvent(new FinalizarAtendimentoCaixa("Finalizar atendimento caixa", getResource(), grupo, getEntitySet(), scheduler)), Scheduler.normal(8, 2));
                 } catch (MathException e) {
                     e.printStackTrace();
                 }
-            }
-
-            else {  //caixa não disponível, coloca grupo na fila
-                getEntitySet().insert(grupo);
             }
 
         }
