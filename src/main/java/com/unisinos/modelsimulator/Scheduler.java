ab426482ec7fe054e3a162b354ae29a913304f80
Derick Rentz <derickrentz@pop-os.localdomain>
2021-06-14 21:39:40 -0300

    Fixing entitySet log not working

--- src/main/java/com/unisinos/modelsimulator/Scheduler.java
+++ src/main/java/com/unisinos/modelsimulator/Scheduler.java
@@ -10,6 +10,7 @@ import org.apache.commons.math.distribution.ExponentialDistributionImpl;
 import org.apache.commons.math.distribution.NormalDistributionImpl;
 
 public class Scheduler {
+
     private double time;
     private List<Event> events;
     private List<Resource> resources;
@@ -68,8 +69,15 @@ public class Scheduler {
     }
 
     protected void executeEvent (Event event) {
+        collectLogs();
         time = event.getEventTime();
         event.execute();
+    }
+
+    private void collectLogs() {
+        entitySets.forEach(
+                EntitySet::logTime
+        );
     }
 
     public void simulateUntil(double absoluteTime) {
@@ -134,7 +142,7 @@ public class Scheduler {
     }
 
     public EntitySet createEntitySet(String name, ArrayList<Entity> entities, int maxPossibleSize) {
-        EntitySet entitySet = new EntitySet(name, entities, maxPossibleSize);
+        EntitySet entitySet = new EntitySet(name, entities, maxPossibleSize, this);
         entitySet.setId(currentId++);
         entitySets.add(entitySet);
         return entitySet;
@@ -174,7 +182,7 @@ public class Scheduler {
     // coleta de estatística
 
     public int getEntityTotalQuantity() {
-        //implement
+        //TODO
         return 0;
     }
 
@@ -183,7 +191,7 @@ public class Scheduler {
     }
 
     public double averageTimeInModel() {
-        //implement
+        //TODO
         return 0;
     }
 
