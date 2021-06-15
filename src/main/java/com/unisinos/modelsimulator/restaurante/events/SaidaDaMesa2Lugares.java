1937eab1b37e660b5282bee0cf3f27b44dcab4f4
Victor Scherer <victorescherer@hotmail.com>
2021-06-14 21:16:32 -0300

    Inicio e Termino PreparoRefeicao

--- src/main/java/com/unisinos/modelsimulator/restaurante/events/SaidaDaMesa2Lugares.java
+++ src/main/java/com/unisinos/modelsimulator/restaurante/events/SaidaDaMesa2Lugares.java
@@ -1,10 +1,12 @@
 package com.unisinos.modelsimulator.restaurante.events;
 
 import com.unisinos.modelsimulator.Event;
+import com.unisinos.modelsimulator.Resource;
+import com.unisinos.modelsimulator.Scheduler;
 
 public class SaidaDaMesa2Lugares extends Event {
 
-  public SaidaDaMesa2Lugares(String name) {
+  public SaidaDaMesa2Lugares(String name, Resource resource, Scheduler scheduler) {
     super(name);
   }
 
--- src/main/java/com/unisinos/modelsimulator/restaurante/events/SaidaDaMesa4Lugares.java
+++ src/main/java/com/unisinos/modelsimulator/restaurante/events/SaidaDaMesa4Lugares.java
@@ -1,10 +1,12 @@
 package com.unisinos.modelsimulator.restaurante.events;
 
 import com.unisinos.modelsimulator.Event;
+import com.unisinos.modelsimulator.Resource;
+import com.unisinos.modelsimulator.Scheduler;
 
 public class SaidaDaMesa4Lugares extends Event {
 
-  public SaidaDaMesa4Lugares(String name) {
+  public SaidaDaMesa4Lugares(String name, Resource resource, Scheduler scheduler) {
     super(name);
   }
 
